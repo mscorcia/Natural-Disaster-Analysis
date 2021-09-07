a234506bba884b8afb79661fab9098c76a2cc4de
Jeferson Stabille <78064878+jstabille@users.noreply.github.com>
2021-09-07 16:45:23 -0400

    Add files via upload

---
+++ Data/scheme_diaster_db.sql
@@ -0,0 +1,51 @@
+﻿
+-- DROP TABLE IF EXISTS USDisaster;
+-- DROP TABLE IF EXISTS USStates;
+-- DROP TABLE IF EXISTS FIPS;
+
+CREATE TABLE public."FPIS"
+(
+    "FIPS_ID" integer NOT NULL,
+    "NAME" character varying NOT NULL,
+    PRIMARY KEY ("FIPS_ID")
+);
+
+
+CREATE TABLE public."USStates"
+(
+    "PostalAbreviation" character varying(2) NOT NULL,
+    "StateDescription" character varying NOT NULL,
+    PRIMARY KEY ("PostalAbreviation")
+);
+
+CREATE TABLE public."USDisaster"
+(
+    fema_declaration character varying NOT NULL,
+    disaster_number integer NOT NULL,
+    state character varying(2) NOT NULL,
+    declaration_type character varying NOT NULL,
+    declaration_date date NOT NULL,
+    fy_declared integer NOT NULL,
+    incident_type character varying NOT NULL,
+    declaration_title character varying NOT NULL,
+    ih_program_declared integer NOT NULL,
+    ia_program_declared integer NOT NULL,
+    pa_program_declared integer NOT NULL,
+    hm_program_declared integer NOT NULL,
+    incident_begin_date date NOT NULL,
+    fips integer NOT NULL,
+    place_code character varying NOT NULL,
+    designated_area character varying NOT NULL,
+    declaration_request_number integer NOT NULL,
+    hash character varying NOT NULL,
+    last_refresh date NOT NULL,
+    id character varying NOT NULL,
+    
+	PRIMARY KEY (id),
+	CONSTRAINT fk_state 
+		FOREIGN KEY ("state") 
+		REFERENCES public."USStates" ("PostalAbreviation"),
+	CONSTRAINT fk_fips 
+		FOREIGN KEY ("fips") 
+		REFERENCES public."FPIS" ("FIPS_ID")
+);
